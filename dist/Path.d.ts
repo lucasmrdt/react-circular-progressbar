67b4d07e9ef78e9a31f13a4f6b80f959f054e43a
Lucas Marandat <lucas.mrdt@gmail.com>
2020-02-25 15:29:25 +0100

    push dist :/

---
+++ dist/Path.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react" />
+declare function Path({ className, counterClockwise, dashRatio, pathRadius, strokeWidth, style, pathRef, }: {
+    className?: string;
+    counterClockwise: boolean;
+    dashRatio: number;
+    pathRadius: number;
+    strokeWidth: number;
+    style?: object;
+    pathRef?: any;
+}): JSX.Element;
+export default Path;
